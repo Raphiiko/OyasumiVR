119fbf8520187fe9aa6fe7b33a9d07ec486e57f1
Raphiiko <iam@raphii.co>
2023-05-05 16:43:27 +0200

    Fix linting

--- src/app/components/base-modal/base-modal.component.ts
+++ src/app/components/base-modal/base-modal.component.ts
@@ -5,6 +5,7 @@ import { Subject } from 'rxjs';
   selector: 'app-base-modal',
   template: ``,
 })
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export class BaseModalComponent<ModalInput extends { [k: string]: any } | void, ModalOutput> {
   result?: ModalOutput;
   close$ = new Subject<void>();
