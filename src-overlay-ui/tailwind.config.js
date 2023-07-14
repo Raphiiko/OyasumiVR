1eaff6729254319ae0c76a5f3ccc81353fb41b38
Raphiiko <iam@raphii.co>
2023-07-14 15:13:07 +0200

    Add proper fonts required for localisation to overlay ui.

--- src-overlay-ui/tailwind.config.js
+++ src-overlay-ui/tailwind.config.js
@@ -1,10 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 export default {
-  mode: 'jit', 
-  purge: ["./src/**/*.svelte"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  mode: "jit", purge: ["./src/**/*.svelte"], theme: {
+    extend: {
+      fontFamily: {
+        sans: ["\"Poppins\"", "\"Noto Sans JP\"", "\"Noto Sans KR\"", "\"Noto Sans TC\"", "\"Noto Sans SC\"", "sans-serif"]
+      }
+    }
+  }, plugins: []
+};
 
