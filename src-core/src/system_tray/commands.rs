e11527cd18fd7a9819f001758efdf1c354c08826
Raphiiko <iam@raphii.co>
2023-07-20 22:08:50 +0200

    Fix rust warning

--- src-core/src/system_tray/commands.rs
+++ src-core/src/system_tray/commands.rs
@@ -3,13 +3,13 @@ use super::{SystemTrayManager, SYSTEMTRAY_MANAGER};
 #[tauri::command]
 pub async fn set_close_to_system_tray(enabled: bool) {
     let mut manager_guard = SYSTEMTRAY_MANAGER.lock().await;
-    let mut manager: &mut SystemTrayManager = manager_guard.as_mut().unwrap();
+    let manager: &mut SystemTrayManager = manager_guard.as_mut().unwrap();
     manager.close_to_tray = enabled;
 }
 
 #[tauri::command]
 pub async fn set_start_in_system_tray(enabled: bool) {
     let mut manager_guard = SYSTEMTRAY_MANAGER.lock().await;
-    let mut manager: &mut SystemTrayManager = manager_guard.as_mut().unwrap();
+    let manager: &mut SystemTrayManager = manager_guard.as_mut().unwrap();
     manager.start_in_tray = enabled;
 }
