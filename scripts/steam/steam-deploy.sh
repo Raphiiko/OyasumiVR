386c9e704496dbb356e788a0602f4c9d461fbddd
Raphiiko <iam@raphii.co>
2023-08-06 17:32:35 +0200

    Install DotNet and AspNetCore during Steam installation
    Update Steam install scripts
    
    
    Update Steam install scripts
    
    
    Update Steam install scripts

--- scripts/steam/steam-deploy.sh
+++ scripts/steam/steam-deploy.sh
@@ -15,12 +15,17 @@ cp src-core/target/release/openvr_api.dll SteamSDK/sdk/tools/ContentBuilder/cont
 cp -r src-core/target/release/resources/ SteamSDK/sdk/tools/ContentBuilder/content/Win64/
 # Include WebView2 Installer
 mkdir -p SteamSDK/sdk/tools/ContentBuilder/content/Win64/WebView2
-curl https://go.microsoft.com/fwlink/p/?LinkId=2124703 --output SteamSDK/sdk/tools/ContentBuilder/content/Win64/WebView2/WebView2RuntimeInstaller.exe
+curl -L https://go.microsoft.com/fwlink/p/?LinkId=2124703 --output SteamSDK/sdk/tools/ContentBuilder/content/Win64/WebView2/WebView2RuntimeInstaller.exe
+# Include DotNet Installer
+mkdir -p SteamSDK/sdk/tools/ContentBuilder/content/Win64/DotNet
+curl -L https://dotnetcli.azureedge.net/dotnet/Runtime/7.0.9/dotnet-runtime-7.0.9-win-x64.exe --output SteamSDK/sdk/tools/ContentBuilder/content/Win64/DotNet/dotnet-runtime-7.0.9-win-x64.exe
+# Include AspNet Installer
+curl -L https://dotnetcli.azureedge.net/dotnet/aspnetcore/Runtime/7.0.9/aspnetcore-runtime-7.0.9-win-x64.exe --output SteamSDK/sdk/tools/ContentBuilder/content/Win64/DotNet/aspnetcore-runtime-7.0.9-win-x64.exe
 # Clear default scripts
 rm SteamSDK/sdk/tools/ContentBuilder/scripts/*
 # Copy over scripts
 cp scripts/steam/scripts/*.vdf SteamSDK/sdk/tools/ContentBuilder/scripts/
-cp scripts/steam/install-scripts/*.vdf SteamSDK/sdk/tools/ContentBuilder/content/
+cp scripts/steam/install-scripts/*.vdf SteamSDK/sdk/tools/ContentBuilder/content/Win64/
 # Replace variables
 sed -i "s/APP_VERSION/$APP_VERSION/" SteamSDK/sdk/tools/ContentBuilder/scripts/*.vdf
 sed -i "s/STEAM_APP_ID/$STEAM_APP_ID/" SteamSDK/sdk/tools/ContentBuilder/scripts/*.vdf
