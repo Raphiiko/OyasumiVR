f68ae76a605355615ecdcf6bcc4083f2dc9824b7
Raphiiko <iam@raphii.co>
2023-09-03 15:32:25 +0200

    Fix linting error

--- scripts/set-build-id.js
+++ scripts/set-build-id.js
@@ -6,8 +6,8 @@ const revision = execSync('git rev-parse --short HEAD').toString().trim();
 {
   let uiFlavour = readFileSync('src-ui/build.ts').toString();
   uiFlavour = uiFlavour.replaceAll(
-    /export const BUILD_ID: string = '([a-zA-Z0-9]|\s)*';/g,
-    `export const BUILD_ID: string = '${revision}';`
+    /export const BUILD_ID = '([a-zA-Z0-9]|\s)*';/g,
+    `export const BUILD_ID = '${revision}';`
   );
   writeFileSync('src-ui/build.ts', uiFlavour);
   console.log('Updated src-ui/build.ts');
