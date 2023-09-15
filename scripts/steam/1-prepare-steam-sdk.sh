61b7defbdfaa5e4670708aa201943f7d662781b1
Raphiiko <iam@raphii.co>
2023-09-15 22:20:22 +0200

    dotnet 7.0.10 -> 7.0.11

--- scripts/steam/1-prepare-steam-sdk.sh
+++ scripts/steam/1-prepare-steam-sdk.sh
@@ -18,8 +18,8 @@ cp SteamSDK/sdk/tools/ContentBuilder/content/Win64/WebView2/WebView2RuntimeInsta
 # Include DotNet Hosting Bundle Installer
 mkdir -p SteamSDK/sdk/tools/ContentBuilder/content/Win64/DotNet
 mkdir -p SteamSDK/sdk/tools/ContentBuilder/content/Win64_CN/DotNet
-curl -L https://download.visualstudio.microsoft.com/download/pr/d489c5d0-4d0f-4622-ab93-b0f2a3e92eed/101a2fae29a291956d402377b941f401/dotnet-hosting-7.0.10-win.exe --output SteamSDK/sdk/tools/ContentBuilder/content/Win64/DotNet/dotnet-hosting-7.0.10-win.exe
-cp SteamSDK/sdk/tools/ContentBuilder/content/Win64/DotNet/dotnet-hosting-7.0.10-win.exe SteamSDK/sdk/tools/ContentBuilder/content/Win64_CN/DotNet/dotnet-hosting-7.0.10-win.exe
+curl -L https://download.visualstudio.microsoft.com/download/pr/d489c5d0-4d0f-4622-ab93-b0f2a3e92eed/101a2fae29a291956d402377b941f401/dotnet-hosting-7.0.11-win.exe --output SteamSDK/sdk/tools/ContentBuilder/content/Win64/DotNet/dotnet-hosting-7.0.11-win.exe
+cp SteamSDK/sdk/tools/ContentBuilder/content/Win64/DotNet/dotnet-hosting-7.0.11-win.exe SteamSDK/sdk/tools/ContentBuilder/content/Win64_CN/DotNet/dotnet-hosting-7.0.11-win.exe
 # Clear default scripts
 rm SteamSDK/sdk/tools/ContentBuilder/scripts/*
 # Copy over scripts
--- src-core/src/globals.rs
+++ src-core/src/globals.rs
@@ -1,7 +1,7 @@
 use tokio::sync::Mutex;
 
-pub const DOTNET_CORE_VERSION: &str = "7.0.10";
-pub const ASPNET_CORE_VERSION: &str = "7.0.10";
+pub const DOTNET_CORE_VERSION: &str = "7.0.11";
+pub const ASPNET_CORE_VERSION: &str = "7.0.11";
 pub const CORE_GRPC_DEV_PORT: u16 = 5176;
 pub const CORE_HTTP_DEV_PORT: u16 = 5177;
 pub const OVERLAY_SIDECAR_GRPC_DEV_PORT: u16 = 5174;
