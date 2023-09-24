df48f9a546d50f5c1fefa19b12bc1515717c259e
Raphiiko <iam@raphii.co>
2023-09-24 18:46:24 +0200

    Improved auto closing of select boxes and brightness control modal

---
+++ src-ui/app/directives/click-outside.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, ElementRef, EventEmitter, HostListener, Output } from '@angular/core';
+
+@Directive({
+  selector: '[clickOutside]',
+})
+export class ClickOutsideDirective {
+  constructor(private elementRef: ElementRef) {}
+
+  @Output() clickOutside = new EventEmitter<MouseEvent>();
+
+  @HostListener('document:click', ['$event', '$event.target'])
+  public onClick(event: MouseEvent, targetElement: HTMLElement): void {
+    if (!targetElement) {
+      return;
+    }
+    const clickedInside = this.elementRef.nativeElement.contains(targetElement);
+    if (!clickedInside) {
+      this.clickOutside.emit(event);
+    }
+  }
+}
