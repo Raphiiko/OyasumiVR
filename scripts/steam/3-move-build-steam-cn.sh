5b25d39a194ce4c218978a764bf691e90ddc7dfb
Raphiiko <iam@raphii.co>
2023-12-26 16:36:56 +0100

    Fix steam builds

--- scripts/steam/3-move-build-steam-cn.sh
+++ scripts/steam/3-move-build-steam-cn.sh
@@ -4,6 +4,7 @@
 
 # Copy files to Win64_CN Depot folder
 cp src-core/target/release/OyasumiVR.exe SteamSDK/sdk/tools/ContentBuilder/content/Win64_CN/
+cp src-core/target/release/flags.toml SteamSDK/sdk/tools/ContentBuilder/content/Win64_CN/
 cp src-core/target/release/openvr_api.dll SteamSDK/sdk/tools/ContentBuilder/content/Win64_CN/
 cp src-core/target/release/steam_api64.dll SteamSDK/sdk/tools/ContentBuilder/content/Win64_CN/
 cp -r src-core/target/release/resources/ SteamSDK/sdk/tools/ContentBuilder/content/Win64_CN/
