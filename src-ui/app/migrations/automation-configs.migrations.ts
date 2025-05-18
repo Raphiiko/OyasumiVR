0a800fe53d3a86df836e81500320b57ca9c93b28
Raphiiko <iam@raphii.co>
2025-05-18 22:32:19 +0200

    Fix migration error

--- src-ui/app/migrations/automation-configs.migrations.ts
+++ src-ui/app/migrations/automation-configs.migrations.ts
@@ -87,11 +87,11 @@ function from17to18(data: any): any {
   if (data.JOIN_NOTIFICATIONS) {
     if (data.JOIN_NOTIFICATIONS.joinSound) {
       data.JOIN_NOTIFICATIONS.joinSoundMode = data.JOIN_NOTIFICATIONS.joinSound;
-      data.joinSound = AUTOMATION_CONFIGS_DEFAULT.JOIN_NOTIFICATIONS.joinSound;
+      data.JOIN_NOTIFICATIONS.joinSound = AUTOMATION_CONFIGS_DEFAULT.JOIN_NOTIFICATIONS.joinSound;
     }
     if (data.JOIN_NOTIFICATIONS.leaveSound) {
       data.JOIN_NOTIFICATIONS.leaveSoundMode = data.JOIN_NOTIFICATIONS.leaveSound;
-      data.leaveSound = AUTOMATION_CONFIGS_DEFAULT.JOIN_NOTIFICATIONS.leaveSound;
+      data.JOIN_NOTIFICATIONS.leaveSound = AUTOMATION_CONFIGS_DEFAULT.JOIN_NOTIFICATIONS.leaveSound;
     }
   }
   if (data.NIGHTMARE_DETECTION.playSound) {
