0af64d58455231921c63d3c9c6381b44ad1f9317
Raphii <iam@raphii.co>
2026-09-17 23:54:53 +0200

    refactor(vrchat): inline friend picture URLs in templates

--- src-ui/app/services/vrchat-api/vrchat.service.ts
+++ src-ui/app/services/vrchat-api/vrchat.service.ts
@@ -280,10 +280,6 @@ export class VRChatService {
     return this.api.getUserGroups(force);
   }
 
-  public imageUrlForPlayer(player: LimitedUserFriend): string | undefined {
-    return player.iconUrl;
-  }
-
   // settings
 
   private async loadSettings(): Promise<void> {
